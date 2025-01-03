d9e02dc5451dc6ef5370b91b15648a482206b922
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-03 01:19:42 +0100

    Create ApplicationCloudConfiguration.java

---
+++ config/ApplicationCloudConfiguration.java
@@ -0,0 +1,18 @@
+package org.softuni.productshop.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+
+@Configuration
+public class ApplicationCloudConfiguration {
+
+    @Value("${cloudinary.cloud-name}")
+    private String cloudApiName;
+    @Value("${cloudinary.api-key}")
+    private String cloudApiKey;
+    @Value("${cloudinary.api-secret}")
+    private String cloudApiSecret;
