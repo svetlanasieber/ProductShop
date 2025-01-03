89cb81bd059f2c8237df538c20318ca6d783a4cb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-03 02:20:14 +0100

    Update ApplicationSecurityConfiguration.java

--- config/ApplicationSecurityConfiguration.java
+++ config/ApplicationSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package productshop.config;
+package org.example.productshop.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
