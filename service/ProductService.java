552ef79456955dddbe18d17124bf195fb69a42c2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-06 02:30:29 +0100

    Update ProductService.java

--- service/ProductService.java
+++ service/ProductService.java
@@ -1,1 +1,21 @@
+package org.example.productshop.service;
+
+import org.softuni.productshop.domain.models.service.ProductServiceModel;
+
+import java.util.List;
+
+public interface ProductService {
+
+    ProductServiceModel createProduct(ProductServiceModel productServiceModel);
+
+    List<ProductServiceModel> findAllProducts();
+
+    ProductServiceModel findProductById(String id);
+
+    ProductServiceModel editProduct(String id, ProductServiceModel productServiceModel);
+
+    void deleteProduct(String id);
+
+    List<ProductServiceModel> findAllByCategory(String category);
+}
 
